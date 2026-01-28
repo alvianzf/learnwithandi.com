8796c94e590eb3037e895cb97f9d7645a560f54b
alvianzf <alvianzf@gmail.com>
2026-01-28 15:36:25 +0700

    squiqly lines

---
+++ npm/src/index.ts
@@ -0,0 +1,6 @@
+// The grand central station of exports. All aboard the squiggly train! 🚂
+
+export { default as SquigglyBackground } from './SquigglyBackground';
+export type { SquigglyBackgroundProps, AnimationVariant } from './SquigglyBackground';
+
+// That's it. That's the whole file. You were expecting more?
